d85e125fc4cd18678c99e89ac1d262a101de99a0
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2017-12-28 15:43:39 +0100

    Add files via upload

--- zadanie5/5.1_Bielecki.c
+++ zadanie5/5.1_Bielecki.c
@@ -15,14 +15,14 @@ int main() {
 	
 	do {
 	printf("Podaj pierwsza godzine (kazda cyfre zatwierdzajac enterem):\n");
-	wypelnianie(godz1);			
+	wypelnianie(godz1, 4);			
 	printf("Podaj druga godzine (kazda cyfre zatwierdzajac enterem):\n");
-	wypelnianie(godz2);	
+	wypelnianie(godz2, 4);	
 	warunek1=check(godz1);
 	warunek2=check(godz2);
 	}while(warunek1!=1 && warunek2!=1);
 	
-	wynik=porownanie(godz1, godz2);
+	wynik=porownanie(godz1, godz2, 4);
 	if(wynik==-1) 
 	{	
 		printf("Godzina %d%d:%d%d jest większa od %d%d:%d%d\n", godz1[0],  godz1[1],  godz1[2],  godz1[3], godz2[0],  godz2[1],  godz2[2],  godz2[3]);
