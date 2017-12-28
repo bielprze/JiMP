1ea2b314606906f586cbe0c9b681a0a0e5c766b2
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2017-12-28 16:33:01 +0100

    Add files via upload

--- zadanie6/6.1_Bielecki.c
+++ zadanie6/6.1_Bielecki.c
@@ -9,13 +9,12 @@
 
 
 int main() {
-
-	printf("Podaj znak którego numer chcesz zobaczyć\n");
-	char a;
-	scanf("%c", &a);
-	
-	int b=a;
-	printf("Twój znak to %c, a jego numer to %d\n", a, b);	
+	unsigned char a;
+	for(int i=32; i<127; i++)
+	{
+		a=i;
+		printf("Liczba to: %d, a znak do niej przyporządkowany to: %c\n", i, a);
+	}
 	
 	return 0;
 }
